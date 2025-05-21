fc210368432b87bdc239dfb0e10351816635e922
Gustavo Caetano de Souza <gustavo.caetano@macbookpro.speedport.ip>
2025-05-21 14:45:39 +0200

    spare parts management

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,11 +14,33 @@ fi
 if [ "$DJANGO_MODE" = "production" ]; then
     echo "Running Production Server"
     python manage.py makemigrations
+    if [ $? -ne 0 ]; then
+      echo "Error: makemigrations failed."
+      exit 1
+    fi
+    
     python manage.py migrate
+    if [ $? -ne 0 ]; then
+      echo "Error: migrate failed."
+      exit 1
+    fi
+
+    echo "Migrations completed successfully."
     gunicorn vsm_tb.wsgi:application --bind 0.0.0.0:8001
 else
     echo "Running Development Server"
     python manage.py makemigrations
+    if [ $? -ne 0 ]; then
+      echo "Error: makemigrations failed."
+      exit 1
+    fi
+    
     python manage.py migrate
+    if [ $? -ne 0 ]; then
+      echo "Error: migrate failed."
+      exit 1
+    fi
+
+    echo "Migrations completed successfully."
     python manage.py runserver 0.0.0.0:8000
 fi
