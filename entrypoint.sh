1dcf6ab881c046186f9273efe224d6f0b2781f6d
Gustavo Caetano de Souza <gustavo.caetano@macbookpro.speedport.ip>
2025-05-21 12:40:08 +0200

    Add Factory Cloud Model

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,12 @@ fi
 # Apply migrations and run server
 if [ "$DJANGO_MODE" = "production" ]; then
     echo "Running Production Server"
+    python manage.py makemigrations
     python manage.py migrate
     gunicorn vsm_tb.wsgi:application --bind 0.0.0.0:8001
 else
     echo "Running Development Server"
+    python manage.py makemigrations
     python manage.py migrate
     python manage.py runserver 0.0.0.0:8000
 fi
