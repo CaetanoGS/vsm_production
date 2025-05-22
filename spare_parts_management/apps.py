04b97e95651c0194892a10aac788c6f83194ae81
Gustavo Caetano de Souza <gustavo.caetano@macbookpro.speedport.ip>
2025-05-22 08:50:03 +0200

    App name changed

--- spare_parts_management/apps.py
+++ spare_parts_management/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SparePartsManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'spare_parts_management'
+    verbose_name = 'Spare Parts Management'
