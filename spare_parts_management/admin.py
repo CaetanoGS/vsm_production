5cb1bcc084dbf5e223dc2da4c2c75c637a99fee2
Gustavo Caetano de Souza <gustavo.caetano@macbookpro.speedport.ip>
2025-05-22 12:45:57 +0200

    Add new categories

--- spare_parts_management/admin.py
+++ spare_parts_management/admin.py
@@ -1,58 +1,30 @@
-from django.contrib import admin, messages
+from django.contrib import admin
 from .models import BackupEquipment
 
 
-@admin.register(BackupEquipment)
 class BackupEquipmentAdmin(admin.ModelAdmin):
-    list_display = (
+    list_display = [
+        "id",
         "name",
-        "location",
         "minimum_quantity",
         "current_quantity",
         "status",
+        "location",
         "producer_name",
         "buyer_name",
-    )
-    readonly_fields = ("status",)
-    list_filter = ("location", "status")
-    fieldsets = (
-        (
-            None,
-            {
-                "fields": (
-                    "name",
-                    "location",
-                    "minimum_quantity",
-                    "current_quantity",
-                    "status",
-                )
-            },
-        ),
-        (
-            "Producer Info",
-            {
-                "fields": (
-                    "producer_name",
-                    "producer_telephone",
-                    "producer_email",
-                )
-            },
-        ),
-        (
-            "Buyer Info",
-            {
-                "fields": (
-                    "buyer_name",
-                    "buyer_email",
-                )
-            },
-        ),
-    )
+    ]
 
-    def change_view(self, request, object_id, form_url="", extra_context=None):
-        obj = self.get_object(request, object_id)
-        if obj and obj.status == "Critical":
-            messages.warning(request, f"Please buy the {obj.name}")
-        return super().change_view(
-            request, object_id, form_url, extra_context=extra_context
-        )
+    def producer_name(self, obj):
+        return obj.producer.name if obj.producer else "-"
+
+    producer_name.admin_order_field = "producer__name"
+    producer_name.short_description = "Producer"
+
+    def buyer_name(self, obj):
+        return obj.buyer.full_name if obj.buyer else "-"
+
+    buyer_name.admin_order_field = "buyer__full_name"
+    buyer_name.short_description = "Buyer"
+
+
+admin.site.register(BackupEquipment, BackupEquipmentAdmin)
