2c72872f0f4826fb2870ac3cadc378a820268ad6
Gustavo Caetano de Souza <gustavo.caetano@macbookpro.speedport.ip>
2025-05-22 15:15:28 +0200

    Add new categories

--- spare_parts_management/models.py
+++ spare_parts_management/models.py
@@ -26,6 +26,30 @@ class BackupEquipment(models.Model):
         ("Stable", "Stable"),
     ]
 
+    TONIEBOX_1 = "Toniebox 1"
+    TONIEBOX_2 = "Toniebox 2"
+    TONIES = "Tonies"
+    BOOK_TONIES = "Book Tonies"
+    CLEVER_TONIES = "Clever Tonies"
+    DISK = "Disk"
+    CONTROLLER = "Controller"
+    NIGHT_LIGHT = "Night Light"
+    SOFT_TONIES = "Soft Tonies"
+    OTHER = "Other"
+
+    CATEGORY_CHOICES = [
+        (TONIEBOX_1, "Toniebox 1"),
+        (TONIEBOX_2, "Toniebox 2"),
+        (TONIES, "Tonies"),
+        (BOOK_TONIES, "Book Tonies"),
+        (CLEVER_TONIES, "Clever Tonies"),
+        (DISK, "Disk"),
+        (CONTROLLER, "Controller"),
+        (NIGHT_LIGHT, "Night Light"),
+        (SOFT_TONIES, "Soft Tonies"),
+        (OTHER, "Other"),
+    ]
+
     name = models.CharField(max_length=255)
     minimum_quantity = models.PositiveIntegerField()
     current_quantity = models.PositiveIntegerField(default=0)
@@ -35,6 +59,13 @@ class BackupEquipment(models.Model):
         Producer, on_delete=models.SET_NULL, null=True, blank=True
     )
     buyer = models.ForeignKey(Buyer, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default=TONIEBOX_2,
+        null=True,
+        blank=True,
+    )
 
     def save(self, *args, **kwargs):
         if self.current_quantity == 0:
--- spare_parts_management/tests.py
+++ spare_parts_management/tests.py
@@ -1,4 +1,4 @@
-from decimal import Decimal
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from tb2_vsm.models import Location
 from .models import Producer, Buyer, BackupEquipment
@@ -30,37 +30,28 @@ class BackupEquipmentModelTests(TestCase):
             full_name="Buyer Y", email="buyery@example.com"
         )
 
+    def create_backup_equipment(self, **kwargs):
+        default_data = {
+            "name": "Test Equipment",
+            "minimum_quantity": 10,
+            "current_quantity": 5,
+            "location": self.location,
+            "producer": self.producer,
+            "buyer": self.buyer,
+        }
+        default_data.update(kwargs)
+        return BackupEquipment.objects.create(**default_data)
+
     def test_status_critical_when_current_quantity_zero(self):
-        be = BackupEquipment.objects.create(
-            name="Equipment 1",
-            minimum_quantity=10,
-            current_quantity=0,
-            location=self.location,
-            producer=self.producer,
-            buyer=self.buyer,
-        )
+        be = self.create_backup_equipment(current_quantity=0)
         self.assertEqual(be.status, "Critical")
 
     def test_status_low_when_current_quantity_below_minimum(self):
-        be = BackupEquipment.objects.create(
-            name="Equipment 2",
-            minimum_quantity=10,
-            current_quantity=5,
-            location=self.location,
-            producer=self.producer,
-            buyer=self.buyer,
-        )
+        be = self.create_backup_equipment(current_quantity=5)
         self.assertEqual(be.status, "Low")
 
     def test_status_stable_when_current_quantity_equal_or_above_minimum(self):
-        be = BackupEquipment.objects.create(
-            name="Equipment 3",
-            minimum_quantity=10,
-            current_quantity=10,
-            location=self.location,
-            producer=self.producer,
-            buyer=self.buyer,
-        )
+        be = self.create_backup_equipment(current_quantity=10)
         self.assertEqual(be.status, "Stable")
 
         be.current_quantity = 15
@@ -68,13 +59,22 @@ class BackupEquipmentModelTests(TestCase):
         self.assertEqual(be.status, "Stable")
 
     def test_str_method_returns_name_and_location_supplier(self):
-        be = BackupEquipment.objects.create(
-            name="Equipment 4",
-            minimum_quantity=10,
-            current_quantity=20,
-            location=self.location,
-            producer=self.producer,
-            buyer=self.buyer,
-        )
-        expected_str = f"Equipment 4 ({self.location.supplier_name})"
+        be = self.create_backup_equipment(name="UPS Unit")
+        expected_str = f"UPS Unit ({self.location.supplier_name})"
         self.assertEqual(str(be), expected_str)
+
+    def test_category_defaults_to_toniebox_2(self):
+        be = self.create_backup_equipment()
+        self.assertEqual(be.category, BackupEquipment.TONIEBOX_2)
+
+    def test_setting_valid_category_choices(self):
+        valid_categories = [choice[0] for choice in BackupEquipment.CATEGORY_CHOICES]
+        for category in valid_categories:
+            be = self.create_backup_equipment(category=category)
+            self.assertEqual(be.category, category)
+
+    def test_invalid_category_raises_error(self):
+        be = self.create_backup_equipment()
+        be.category = "InvalidCategory"
+        with self.assertRaises(ValidationError):
+            be.full_clean()
