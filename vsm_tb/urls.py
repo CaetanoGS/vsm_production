03e44b43cad15915e77717fce860291f581d1938
Gustavo Caetano de Souza <gustavo.caetano@macbookpro.speedport.ip>
2025-06-13 08:35:43 +0200

    Redirect admin panel

--- vsm_tb/urls.py
+++ vsm_tb/urls.py
@@ -19,8 +19,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+def redirect_to_admin(request):
+    return redirect("/admin/")
+
 
 urlpatterns = [
+    path("", redirect_to_admin),
     path("admin/", admin.site.urls),
 ]
 
