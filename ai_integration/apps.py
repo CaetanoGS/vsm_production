ece62ea8f2acf9b32bb3e314620c152c16c70199
Gustavo Caetano de Souza <gustavo.caetano@macbookpro.speedport.ip>
2025-06-20 14:45:10 +0200

    Remove AI integration

--- ai_integration/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AiIntegrationConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "ai_integration"
-    verbose_name = "WIP - ChatGPT Integration"
--- vsm_tb/settings.py
+++ vsm_tb/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     "django_countries",
     "spare_parts_management",
     "maintenance",
-    "ai_integration",
 ]
 
 MIDDLEWARE = [
